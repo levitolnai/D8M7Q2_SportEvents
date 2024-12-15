93b3c231833fa4445a09149788a4972b6435a6b2
levitolnai <levi.tolnai7@gmail.com>
2024-12-15 19:36:40 +0100

    UserCreateDto added

---
+++ D8M7Q2_SportEvents.Entities/Dto/User/UserCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8M7Q2_SportEvents.Entities.Dto.User
+{
+    public class UserCreateDto
+    {
+        [MinLength(6)]
+        public required string UserName { get; set; } = "";
+
+        [MinLength(6)]
+        public required string Password { get; set; } = "";
+    }
+}
---
+++ D8M7Q2_SportEvents/Controller/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace D8M7Q2_SportEvents.Endpoint.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public void Register()
+        {
+            
+
+
+        }
+    }
+}
