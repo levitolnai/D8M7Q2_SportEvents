443d0d24c7931935ed15c424c4e16b88af441cc4
levitolnai <levi.tolnai7@gmail.com>
2024-12-15 14:58:41 +0100

    automapper init

---
+++ D8M7Q2_SportEvents.Logic/DtoProvider.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using D8M7Q2_SportEvents.Entities;
+using D8M7Q2_SportEvents.Entities.Dto.SportEvent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8M7Q2_SportEvents.Logic
+{
+    public class DtoProvider
+    {
+        public Mapper Mapper { get; }
+
+        public DtoProvider()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<SportEvent, SportEventShortViewDto>();
+                cfg.CreateMap<SportEvent, SportEventViewDto>();
+                cfg.CreateMap<SportEventCreateUpdateDto, SportEvent>();
+            });
+
+            Mapper = new Mapper(config);
+        }
+    }
+}
