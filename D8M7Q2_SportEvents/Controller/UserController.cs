f4cb1247ba85cb2c162ad231b00f03b2fd356076
levitolnai <levi.tolnai7@gmail.com>
2024-12-15 19:47:28 +0100

    Login action

--- D8M7Q2_SportEvents/Controller/UserController.cs
+++ D8M7Q2_SportEvents/Controller/UserController.cs
@@ -1,6 +1,10 @@
 ﻿using D8M7Q2_SportEvents.Entities.Dto.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace D8M7Q2_SportEvents.Endpoint.Controller
 {
@@ -28,19 +32,40 @@ namespace D8M7Q2_SportEvents.Endpoint.Controller
             var user = await userManager.FindByNameAsync(dto.UserName);
             if (user == null)
             {
-                throw new ArgumentException("User not found");
+                throw new ArgumentException("User not found!");
             }
             else
             {
                 var result = await userManager.CheckPasswordAsync(user, dto.Password);
                 if (!result)
                 {
-                    throw new ArgumentException("Incorrect password");
+                    throw new ArgumentException("Incorrect password!");
                 }
                 else
                 {
+                    var claim = new List<Claim>();
+                    claim.Add(new Claim(ClaimTypes.Name, user.UserName!));
+                    claim.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
 
+                    var token = GenerateAccessToken(claim);
                 }
             }
         }
+
+        private JwtSecurityToken GenerateAccessToken(IEnumerable<Claim>? claims)
+        {
+            var signinKey = new SymmetricSecurityKey(
+                  Encoding.UTF8.GetBytes("Nagyonhosszútitkosítókulcs"));
+
+            int expiryInMinutes = 24 * 60;
+
+            return new JwtSecurityToken(
+                  issuer: "sportevent.com",
+                  audience: "sportevent.com",
+                  claims: claims?.ToArray(),
+                  expires: DateTime.Now.AddMinutes(expiryInMinutes),
+                  signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+        }
+    }
 }
