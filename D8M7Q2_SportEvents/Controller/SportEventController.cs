9e096296ff8cfcde5bdc62f8d52a9084887488a6
levitolnai <levi.tolnai7@gmail.com>
2024-12-15 13:04:27 +0100

    test controller

---
+++ D8M7Q2_SportEvents/Controller/SportEventController.cs
@@ -0,0 +1,28 @@
+﻿using D8M7Q2_SportEvents.Data;
+using D8M7Q2_SportEvents.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D8M7Q2_SportEvents.Endpoint.Controller
+{
+    public class SportEventController
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class MovieController : ControllerBase
+        {
+            SportEventContext ctx;
+
+            public MovieController(SportEventContext ctx)
+            {
+                this.ctx = ctx;
+            }
+
+            [HttpPost]
+            public void AddMovie(SportEvent sportEvent)
+            {
+                ctx.SportEvents.Add(sportEvent);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
