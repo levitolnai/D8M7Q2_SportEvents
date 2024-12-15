4349dcd9c9aa87151fdf664f43645dea4999b380
levitolnai <levi.tolnai7@gmail.com>
2024-12-15 12:39:51 +0100

    SportEvenet, Competitor added

---
+++ D8M7Q2_SportEvents.Entities/Competitor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8M7Q2_SportEvents.Entities
+{
+    public class Competitor
+    {
+        public Competitor(string sportEventId, string name, string email, string phone)
+        {
+            Id = Guid.NewGuid().ToString();
+            SportEventId = sportEventId;
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+
+        [StringLength(50)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [StringLength(50)]
+        public string SportEventId { get; set; }
+
+        [NotMapped]
+        public virtual SportEvent? SportEvent { get; set; }
+
+        [StringLength(60)]
+        public string Name { get; set; }
+
+        [StringLength(40)]
+        public string Email { get; set; }
+
+        [StringLength(15)]
+        public string Phone { get; set; }
+    }
+}
---
+++ D8M7Q2_SportEvents.Entities/SportEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace D8M7Q2_SportEvents.Entities
+{
+    public class SportEvent
+    {
+        public SportEvent(string title, string date, int applicantLimit)
+        {
+            Id = Guid.NewGuid().ToString();
+            Title = title;
+            Date = date;
+            ApplicantLimit = applicantLimit;
+        }
+
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Date { get; set; }
+        public int ApplicantLimit { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Competitor>? Competitors { get; set; }
+    }
+}
