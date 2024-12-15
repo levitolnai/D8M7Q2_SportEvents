f885e89ca0f0525cb20ce119f9e281573d2952e8
levitolnai <levi.tolnai7@gmail.com>
2024-12-15 12:44:55 +0100

    DbContext done

--- D8M7Q2_SportEvents.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace D8M7Q2_SportEvents.Data
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ D8M7Q2_SportEvents.Data/SportEventContext.cs
@@ -0,0 +1,29 @@
+﻿using D8M7Q2_SportEvents.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace D8M7Q2_SportEvents.Data
+{
+    public class SportEventContext: DbContext
+    {
+        public DbSet<SportEvent> SportEvents { get; set; }
+
+        public DbSet<Competitor> Competitors { get; set; }
+
+        public SportEventContext(DbContextOptions<SportEventContext> ctx)
+            : base(ctx)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SportEvent>()
+                .HasMany(m => m.Competitors)
+                .WithOne(r => r.SportEvent)
+                .HasForeignKey(r => r.SportEventId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
